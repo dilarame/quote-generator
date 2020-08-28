c7a3f00014db75b5296874c672b74a7eac6e5a20
dilarame <mukayeva.dilara@gmail.com>
2020-08-28 15:54:01 +0100

    Remove potato spaces

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const  quoteContainer = document.getElementById('quote-container');
-const  quoteText = document.getElementById('quote');
-const  authorText = document.getElementById('author');
-const  twitterBtn = document.getElementById('twitter');
-const  newQuoteBtn = document.getElementById('new-quote');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
 function showLoadingSpinner() {
@@ -20,7 +20,6 @@ function removeLoadingSpinner() {
 var count = 0;
 
 // Get Quote from API
-
 async function getQuote() {
     showLoadingSpinner();
     const proxyUrl = 'https://gentle-waters-49097.herokuapp.com/'
