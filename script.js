b0a32fb75429144b98a9b1b4452e45d02714a4c7
dilarame <mukayeva.dilara@gmail.com>
2020-08-28 15:21:04 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const  quoteContainer = document.getElementById('quote-container');
+const  quoteText = document.getElementById('quote');
+const  authorText = document.getElementById('author');
+const  twitterBtn = document.getElementById('twitter');
+const  newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if(!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+var count = 0;
+
+// Get Quote from API
+
+async function getQuote() {
+    showLoadingSpinner();
+    const proxyUrl = 'https://gentle-waters-49097.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=ru&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // if author is blank, enter 'Unknown'.
+        if(data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // reduce size of font for long quotes
+        if(data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+
+        // stop loader and show the quote
+        removeLoadingSpinner();
+    } catch (error) {
+        if(count < 10) {
+            count++;
+            getQuote();
+        } 
+    }
+}
+
+// Tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On load
+getQuote();
